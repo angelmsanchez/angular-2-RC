8a72cac41c0b78a6abbd725b7fa74291386a9982
angel <angelmsanchez85@gmail.com>
2016-10-13 09:51:16 +0200

    delete files config

--- tools/env/base.ts
+++
@@ -1,8 +0,0 @@
-// import { EnvConfig } from './env-config.interface';
-
-// const BaseConfig: EnvConfig = {
-//   API_LOCALHOST: 'http://localhost:4444/',
-//   API_FILMS: 'http://localhost:5555/'
-// };
-
-// export = BaseConfig;
