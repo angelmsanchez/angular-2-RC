b3142dd00ef44b24cf5c8fd40339ac7bd49534bf
DESKTOP-85QIP76\consultant <angelmsanchez85@gmail.com>
2016-11-28 16:26:58 +0100

    content components

---
+++ src/client/app/shared/components/counter/counter.component.ts
@@ -0,0 +1,38 @@
+import { Component, Inject } from '@angular/core';
+import { Store } from 'redux';
+
+import { AppState } from './../../../redux/app-state';
+import { AppStore } from './../../../redux/app-store';
+import * as CounterActions from './../../../redux/counter-action-creators';
+
+@Component({
+    moduleId: module.id,
+    selector: 'app-counter',
+    templateUrl: 'counter.html'
+
+})
+export class CounterComponent {
+    counter: number;
+
+    constructor( @Inject(AppStore) private store: Store<AppState>) {
+        store.subscribe(() => this.readState());
+        this.readState();
+    }
+
+    readState() {
+        let state: AppState = this.store.getState() as AppState;
+        this.counter = state.counter;
+    }
+
+    increment() {
+        this.store.dispatch(CounterActions.increment());
+    }
+
+    decrement() {
+        this.store.dispatch(CounterActions.decrement());
+    }
+
+    restart() {
+        this.store.dispatch(CounterActions.restart());
+    }
+}
